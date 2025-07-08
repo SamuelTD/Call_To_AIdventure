26e70d173a0b64dddd7dfa1bceb4622942e0525b
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-07-08 09:12:25 +0200

    fix : removed GROQ API KEY FROM CODE

--- src/agents/game_master_2.py
+++ src/agents/game_master_2.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
 import chromadb
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain_chroma import Chroma
@@ -12,66 +11,45 @@ from langchain_core.runnables import RunnablePassthrough
 # Chroma collections
 CHAR_COL = "characters"
 LOC_COL = "locations"
+OTHER_COL = "other"
 
 # Le modèle d'embedding (doit être le même que celui utilisé pour la création).
 EMBEDDING_MODEL = "mxbai-embed-large:latest"
 
 # Le modèle de LLM à utiliser pour la génération de la réponse.
-LLM_MODEL = "llama3.2:latest"
+LLM_MODEL = "artifish/llama3.2-uncensored"
 
 # Initialise le client Ollama pour les embeddings
 ollama_embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL)
 
 # Vector stores 
 
-vectorstore_char = Chroma(
+vectorstore = Chroma(
     client=chromadb.PersistentClient(path="db/chroma"),
-    collection_name=CHAR_COL,
-    embedding_function=ollama_embeddings
-)
-
-vectorstore_loc = Chroma(
-    client=chromadb.PersistentClient(path="db/chroma"),
-    collection_name=LOC_COL,
+    collection_name=OTHER_COL,
     embedding_function=ollama_embeddings
 )
 
 # Retrievers
-lulu_retriever = vectorstore_char.as_retriever(search_kwargs={"k":1, "filter": {
+sonya_retriever = vectorstore.as_retriever(search_kwargs={"k":1, "filter": {
             "$and": [
                 {"type": {"$eq": "character"}},
-                {"id":   {"$eq": "lulu_the_wise"}}
+                {"id":   {"$eq": "sonya"}}
             ]
         }})
 
-char_retriever = vectorstore_char.as_retriever(search_kwargs={"k": 5, "filter": {"id" : {"$ne": "lulu_the_wise"}}})
-
-loc_retriever = vectorstore_loc.as_retriever(search_kwargs={"k": 3})
-
 # Model with OLLAMA
-<<<<<<< HEAD
 llm = ChatOllama(model=LLM_MODEL)
-=======
-# llm = ChatOllama(model=LLM_MODEL)
-llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"), model="llama-3.3-70b-versatile")
-
-# Memory
-history = []
->>>>>>> d1bb64d (fix : removed GROQ API KEY FROM CODE)
 
 # Prompt Template
 
 template = """
-{lulu}
+{sonya}
 
-Now, using those scrolls *and* the following additional world‐lore:
+Using Sonya's personnality, background and traits, answer the user input as best as you can. You will be explicit and sexual but classy and refined. \
+    You are always willing to have sex. Use * * to narrate your actions. For example *sink to my knees and wrap my lips around your cock*.
 
-{context_char}
-{context_loc}
-
-Answer with Lulu’s trademark sass…
-
-Question: {question}
+User input: {question}
 """
 
 prompt = ChatPromptTemplate.from_template(template)
@@ -79,7 +57,7 @@ prompt = ChatPromptTemplate.from_template(template)
 # RAG chain
 
 rag_chain = (
-    {"lulu": lulu_retriever, "context_char": char_retriever, "context_loc": loc_retriever, "question": RunnablePassthrough()} # Étape de recherche (Retrieval)
+    {"sonya": sonya_retriever, "question": RunnablePassthrough()} # Étape de recherche (Retrieval)
     | prompt                                                  # Étape d'augmentation (Augmented)
     | llm                                                     # Étape de génération (Generation)
     | StrOutputParser()                                       # Parse la sortie du LLM en chaîne de caractères
@@ -93,7 +71,7 @@ if __name__ == "__main__":
     # print("Retrieved chunks: ", doc)
     
     print("\n--- Game Master v0.1 ---")
-    print("Ask question about a known character.")
+    print("You're discussing with Sonya.")
 
     
     while True:
@@ -101,7 +79,7 @@ if __name__ == "__main__":
         if user_question.lower() == "exit":
             break
 
-        print("Assistant: ...")
+        
         # Invoque la chaîne RAG avec la question de l'utilisateur
         answer = rag_chain.invoke(user_question)
         print(f"\rAssistant: {answer}")
